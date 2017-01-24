23d92af5dc4dbfd4c9f30e06e0d15631053ca7b4
bouiboui <phpman1@gmail.com>
2017-01-24 11:17:30 +0100

    Created function-specific deployment script

---
+++ generators/app/templates/bin/deploy-function.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+###
+# Deploy the function passed in the first argument (for development)
+##
+
+source $(dirname $0)/function-lib.sh
+
+DEFAULT_AWS_REGION=eu-west-1
+DEFAULT_STAGE=dev
+
+cd $(dirname $0)/../
+
+# Remove devDependencies
+callFunction "Remove devDependencies" "npm prune --production"
+# Install production dependencies
+callFunction "Install production dependencies" "npm install --production"
+# Deploy Serverless project
+callFunction "Deploy Serverless project" "sls deploy function --function ${1}  --verbose --region ${DEFAULT_AWS_REGION} --stage dev"
+
+# EXIT
+exit ${RETURN_CODE}
